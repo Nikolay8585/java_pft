1d60809205d417f6452a598bbe3e55f7afb28d97
NikolayMalygin <malygin-era@mail.ru>
2018-04-18 15:35:12 +0300

    доьавлена проверка на пустые поля заполнения

--- addresbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addresbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -4,11 +4,10 @@ import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
-//import org.openqa.selenium.remote.BrowserType;
 
 public class TestBase {
 
-    protected final ApplicationManager app = new ApplicationManager(BrowserType.EDGE);
+    protected final ApplicationManager app = new ApplicationManager(BrowserType.FIREFOX);
 
     @BeforeMethod
     public void setUp() throws Exception {
