466950b22d1cf4ee7cda63cebb10fc17c2fae43e
NikolayMalygin <malygin-era@mail.ru>
2018-05-25 19:19:16 +0300

    Добавление контактов в группу с условиями

--- addresbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddToGroupTests.java
+++ addresbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactAddToGroupTests.java
@@ -1,6 +1,7 @@
 package ru.stqa.pft.addressbook.tests;
 
 
+import org.hibernate.AssertionFailure;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.*;
@@ -18,22 +19,27 @@ public class ContactAddToGroupTests extends TestBase {
     public void testContactAddToGroup() {
 
         Groups groups = app.db().groups();
+        if (groups.size() == 0) {
+            app.goTo().groupPage();
+            app.group().create(new GroupData().withName("test4").withHeader("test4").withFooter("test4"));
+            groups = app.db().groups();
+        }
         GroupData group = groups.iterator().next();
         Contacts before = app.db().contacts();
         ContactData contact = before.iterator().next();
         ContactInGroupData contactInGroup = new ContactInGroupData()
                 .withContactId(contact.getId())
                 .withGroupId(group.getId());
-        //File photo = new File("src/test/resources/smile.png");
-        /*ContactData newContact = new ContactData()
-                .withFirstName("Toto").withLastName("Mursikov")
-                .withPhoneHome("111").withEMail("11@mail.ru").inGroup(groups.iterator().next()).withAddress("Moscow");
-
-         */
+        ContactsInGroups beforeContactsInGroups = app.db().contactsInGroups();
+        if (beforeContactsInGroups.contains(contactInGroup)) {
+            app.goTo().groupPage();
+            app.group().create(group.withName("test5").withHeader("test5").withFooter("test5"));
+        }
         app.goTo().homePage();
         app.contact().addToGroup(contact, group);
         app.goTo().homePage();
-        ContactsInGroups contactsInGroups = app.db().contactsInGroups();
-        contactsInGroups.contains(contactInGroup);
+        ContactsInGroups afterContactsInGroups = app.db().contactsInGroups();
+        assertThat(true, equalTo(afterContactsInGroups.contains(contactInGroup)));
+
     }
 }
